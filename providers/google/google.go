efc2491409ce678526924dd6c11e7847a2dd133f
Diego Borges <drborges.cic@gmail.com>
2015-02-24 17:16:50 -0300

    Runs gofmt on the repo

--- providers/google/google.go
+++ providers/google/google.go
@@ -1,80 +1,80 @@
 package google
 
 import (
-    "net/http"
-    "github.com/drborges/geocoder"
-    "encoding/json"
-    "io/ioutil"
-    "fmt"
+	"encoding/json"
+	"fmt"
+	"github.com/drborges/geocoder"
+	"io/ioutil"
+	"net/http"
 )
 
 const (
-    ReverseGeocodeEndpoint = "https://maps.googleapis.com/maps/api/geocode/json?latlng=%v,%v"
+	ReverseGeocodeEndpoint = "https://maps.googleapis.com/maps/api/geocode/json?latlng=%v,%v"
 )
 
 type Geocoder struct {
-    HttpClient             *http.Client
-    ReverseGeocodeEndpoint string
+	HttpClient             *http.Client
+	ReverseGeocodeEndpoint string
 }
 
 func NewGeocoder() geocoder.Geocoder {
-    return &Geocoder{
-        HttpClient: &http.Client{},
-        ReverseGeocodeEndpoint: ReverseGeocodeEndpoint,
-    }
+	return &Geocoder{
+		HttpClient:             &http.Client{},
+		ReverseGeocodeEndpoint: ReverseGeocodeEndpoint,
+	}
 }
 
 func NewGeocoderWithHttpProvider(c *http.Client) geocoder.Geocoder {
-    return &Geocoder{
-        HttpClient: c,
-        ReverseGeocodeEndpoint: ReverseGeocodeEndpoint,
-    }
+	return &Geocoder{
+		HttpClient:             c,
+		ReverseGeocodeEndpoint: ReverseGeocodeEndpoint,
+	}
 }
 
 func (geo *Geocoder) ReverseGeocode(lat float64, lng float64) (*http.Response, error) {
-    return geo.HttpClient.Get(fmt.Sprintf(geo.ReverseGeocodeEndpoint, lat, lng))
+	return geo.HttpClient.Get(fmt.Sprintf(geo.ReverseGeocodeEndpoint, lat, lng))
 }
 
 func AddressMapper(res *http.Response) (geocoder.Address, error) {
-    if body, err := ioutil.ReadAll(res.Body); err == nil {
-        r := new(Response)
-        json.Unmarshal(body, &r)
-        return r.address(), nil
-    } else {
-        return geocoder.EmptyAddress, err
-    }
+	if body, err := ioutil.ReadAll(res.Body); err == nil {
+		r := new(Response)
+		json.Unmarshal(body, &r)
+		return r.address(), nil
+	} else {
+		return geocoder.EmptyAddress, err
+	}
 }
 
 type QueryResults struct {
-    Types             []string            `json:"types"`
-    AddressComponents []AddressComponents `json:"address_components"`
+	Types             []string            `json:"types"`
+	AddressComponents []AddressComponents `json:"address_components"`
 }
 
 type AddressComponents struct {
-    LongName  string   `json:"long_name"`
-    ShortName string   `json:"short_name"`
-    Types     []string `json:"types"`
+	LongName  string   `json:"long_name"`
+	ShortName string   `json:"short_name"`
+	Types     []string `json:"types"`
 }
 
 type Response struct {
-    Results []QueryResults `json:"results"`
+	Results []QueryResults `json:"results"`
 }
 
 func (res *Response) address() geocoder.Address {
-    address := geocoder.Address{}
-    for _, result := range res.Results {
-        if result.Types[0] == "locality" {
-            for _, addrComponent := range result.AddressComponents {
-                if addrComponent.Types[0] == "locality" {
-                    address.City = addrComponent.ShortName
-                } else if addrComponent.Types[0] == "administrative_area_level_1" {
-                    address.State = addrComponent.ShortName
-                } else if addrComponent.Types[0] == "country" {
-                    address.Country = addrComponent.ShortName
-                }
-            }
-        }
-    }
+	address := geocoder.Address{}
+	for _, result := range res.Results {
+		if result.Types[0] == "locality" {
+			for _, addrComponent := range result.AddressComponents {
+				if addrComponent.Types[0] == "locality" {
+					address.City = addrComponent.ShortName
+				} else if addrComponent.Types[0] == "administrative_area_level_1" {
+					address.State = addrComponent.ShortName
+				} else if addrComponent.Types[0] == "country" {
+					address.Country = addrComponent.ShortName
+				}
+			}
+		}
+	}
 
-    return address
+	return address
 }
--- providers/google/google_test.go
+++ providers/google/google_test.go
@@ -1,47 +1,47 @@
 package google
 
 import (
-    "testing"
-    "github.com/stretchr/testify/assert"
-    "github.com/drborges/geocoder/providers/google"
-    "net/http"
+	"github.com/drborges/geocoder/providers/google"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
 )
 
 func TestReverseGeocodeFromNewGoogleGeocoder(t *testing.T) {
-    geocoder := google.NewGeocoder()
+	geocoder := google.NewGeocoder()
 
-    res, _ := geocoder.ReverseGeocode(47.6064, -122.330803)
+	res, _ := geocoder.ReverseGeocode(47.6064, -122.330803)
 
-    address, _ := google.AddressMapper(res)
+	address, _ := google.AddressMapper(res)
 
-    assert.Equal(t, "Seattle", address.City)
-    assert.Equal(t, "WA", address.State)
-    assert.Equal(t, "US", address.Country)
+	assert.Equal(t, "Seattle", address.City)
+	assert.Equal(t, "WA", address.State)
+	assert.Equal(t, "US", address.Country)
 }
 
 func TestReverseGeocodeFromNewGoogleGeocoderWithHttpProvider(t *testing.T) {
-    geocoder := google.NewGeocoderWithHttpProvider(&http.Client{})
+	geocoder := google.NewGeocoderWithHttpProvider(&http.Client{})
 
-    res, _ := geocoder.ReverseGeocode(47.6064, -122.330803)
+	res, _ := geocoder.ReverseGeocode(47.6064, -122.330803)
 
-    address, _ := google.AddressMapper(res)
+	address, _ := google.AddressMapper(res)
 
-    assert.Equal(t, "Seattle", address.City)
-    assert.Equal(t, "WA", address.State)
-    assert.Equal(t, "US", address.Country)
+	assert.Equal(t, "Seattle", address.City)
+	assert.Equal(t, "WA", address.State)
+	assert.Equal(t, "US", address.Country)
 }
 
 func TestReverseGeocodeFromGoogleCoder(t *testing.T) {
-    geocoder := google.Geocoder{
-        HttpClient: &http.Client{},
-        ReverseGeocodeEndpoint: "https://maps.googleapis.com/maps/api/geocode/json?latlng=%v,%v",
-    }
+	geocoder := google.Geocoder{
+		HttpClient:             &http.Client{},
+		ReverseGeocodeEndpoint: "https://maps.googleapis.com/maps/api/geocode/json?latlng=%v,%v",
+	}
 
-    res, _ := geocoder.ReverseGeocode(47.6064, -122.330803)
+	res, _ := geocoder.ReverseGeocode(47.6064, -122.330803)
 
-    address, _ := google.AddressMapper(res)
+	address, _ := google.AddressMapper(res)
 
-    assert.Equal(t, "Seattle", address.City)
-    assert.Equal(t, "WA", address.State)
-    assert.Equal(t, "US", address.Country)
+	assert.Equal(t, "Seattle", address.City)
+	assert.Equal(t, "WA", address.State)
+	assert.Equal(t, "US", address.Country)
 }
